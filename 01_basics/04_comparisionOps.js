2edd6994a265f9f97b72cb2981ca02de32f3c30c
Abhishek Kumar <absk.devopsmastero@gmail.com>
2025-06-21 08:17:35 +0000

    Comparision ops of datatypes

---
+++ 01_basics/04_comparisionOps.js
@@ -0,0 +1,23 @@
+// BASIC COMPARISION OPERATION IN BOOLEAN FORM
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//TRICK COMPARISION : Unpredictable results becoz of lang inconsistency issues
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// unknown that null convert to nan or 0
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//similarly undefined false in all cases
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === => strict assignment operator CHECK THE DT OF VAR ALSO FOR COMP
+//console.log("2" === 2);
