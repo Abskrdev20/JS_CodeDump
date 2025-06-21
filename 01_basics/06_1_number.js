b2c3f56aa990f5aea0eb2a74e676f346eb890752
Abhishek Kumar <absk.devopsmastero@gmail.com>
2025-06-21 16:19:18 +0000

    Number Methods in JS

---
+++ 01_basics/06_1_number.js
@@ -0,0 +1,18 @@
+const num = 294
+console.log(num)
+
+const newNum= new Number(387.84);
+
+console.log(newNum.toString().length); //to apply string prebuilt methods
+console.log(newNum.toFixed(2)); // upto which decimal point
+console.log(newNum.toPrecision(3)); // round off to the significant figures
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString("en-IN")); // repsnt large nos in IN standards
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+
+
+
