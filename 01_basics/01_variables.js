a4300d69a8de1f2d5157f485c63b1e29141a9a0c
Abhishek Kumar <absk.devopsmastero@gmail.com>
2025-06-21 07:01:19 +0000

    datatypes done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,23 @@ const acctId=14453
 let acctEmail="akdev@gmail.com"
 var acctPass="12345"
 acctCity ="Delhi"
+let acctState
 
-acctId=5
-console.log(acctId);
+//ASSIGNMENT TO THE CONST IS NOT ALLOWED
+// acctId=5
+// console.log(acctId);
+console.log(acctId); //default print
+
+
+acctEmail="vidhimlops@gmail.com"
+acctPass="54321"
+acctCity ="Jaipur"
+
+console.table([acctId,acctEmail,acctPass,acctCity,acctState]);  //tabular form mutiple variables print on console
+
+/*
+NOTE : 
+1. It is preferred not to use var keyword in modern days becoz o fissue in func. & block scope
+2. We can have uninitialed variable alse
+*/
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"
+
+//alert(3+3) not allowed as cwe are using nodejs not browser
+
+// Primitive data types
+//number
+let num=10;
+//bigInt => for larger values of numbers
+//string
+let str="hello Guest"
+//boolean=> true/false
+let isprime=true
+//null => standalone value shows the absence of value
+let showWeather=null
+//undefined=> var value is uninitialised
+let name
+// symbol => in components repsnt Uniqueness
+
+//object
+
+//typeof => tells the type of variable or val
+console.log(typeof "hello!! coders");
+console.log(typeof isprime);
+console.log(typeof 33);
+/* spl :
+1 . typeof null is obj
+2 . typeof undefined is undefined
+*/ 
+console.log(typeof showWeather);
+console.log(typeof undefined);
+
+
+
+
+
