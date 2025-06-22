5920c288cbffae8a91e1dfdd1b1a2cad3aec16a8
Abhishek Kumar <absk.devopsmastero@gmail.com>
2025-06-22 10:38:23 +0000

    Object Part 2 Updated

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -11,13 +11,10 @@ const user = {
 }
 // console.log(user.username.fullusername.firstname);
 //methods
-console.log(Object.keys(user)); // return array of keys
-console.log(Object.values(user)); // ... values
-console.log(Object.entries(user));
-console.log(user.hasOwnProperty("email"));
-
-
-
+// console.log(Object.keys(user)); // return array of keys
+// console.log(Object.values(user)); // ... values
+// console.log(Object.entries(user));
+// console.log(user.hasOwnProperty("email"));
 
 
 // Mutiple Objects merging into single
@@ -33,3 +30,21 @@ const obj3 = { 5 : "e",6 : "f"}
 
 // console.log(obj4);
 
+// Destructuring Of obj
+const course = {
+    coursename : "Learn JS",
+    price : 3433,
+    courseInstructor : "abhijeet malhan"
+}
+// course.courseInstructor
+const {courseInstructor : teacher} = course
+console.log(teacher);
+
+// Understanding APIs
+// API CALLS => return the code in JSON FORMAT earlier in xml format
+// json : javascript object notation
+// {
+//     "fruit": "Apple",
+//     "size": "Large",
+//     "color": "Red"
+// }
