fd98abe205b00e240cbd31d8c82d5d782d2bf9f8
Abhishek Kumar <absk.devopsmastero@gmail.com>
2025-06-22 10:15:47 +0000

    Objects part 2 done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,35 @@
+
+// Object chaining
+const user = {
+    email : "roshan@gmail.com",
+    username : {
+        fullusername : {
+            firstname : "roshan",
+            lastname : "rajput"
+        }
+    }
+}
+// console.log(user.username.fullusername.firstname);
+//methods
+console.log(Object.keys(user)); // return array of keys
+console.log(Object.values(user)); // ... values
+console.log(Object.entries(user));
+console.log(user.hasOwnProperty("email"));
+
+
+
+
+
+// Mutiple Objects merging into single
+const obj1 = { 1 : "a",2 : "b"}
+const obj2 = { 3 : "c",4 : "d"}
+const obj3 = { 5 : "e",6 : "f"}
+
+// const obj4 = {obj1,obj2,obj3} // get multi dim objects
+// individually add
+// const obj4 = Object.assign({},obj1,obj2,obj3) 
+// Most used approach
+// const obj4 = {...obj1,...obj2,...obj3}
+
+// console.log(obj4);
+
