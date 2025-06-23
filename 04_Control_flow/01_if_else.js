9402f0cdfbffbc0e84ec2a5a941c2626d5df091f
Abhishek Kumar <absk.devopsmastero@gmail.com>
2025-06-23 11:16:21 +0000

    If-else conditional statements

---
+++ 04_Control_flow/01_if_else.js
@@ -0,0 +1,45 @@
+// CONTROL FLOW OR LOGIC FLOW
+// if - else
+
+// const num =10
+// if (num%2==0) {
+//     console.log(`Entered ${num} is an Even no`);
+// } else {
+//     console.log(`Entered ${num} is an Odd no`);  
+// }
+
+// Implicit Scope Notation for single line control stat
+// let  temp = 50 ;
+// if (temp>25) console.log(`Its a Hot day`);
+
+// correct but false practise
+// if (temp ==50) console.log("Its an exteme hot day"),console.log(`Today is the warmest day of the year`);
+
+// Nested if-elseif-else
+const bal = 500
+// if(bal<250){
+//     console.log("less than 250");
+// } else if (bal<500) {
+//     console.log("less than 500");
+// } else if (bal<750) {
+//     console.log("less than 750");
+// } else {
+//     console.log("less than 1000");
+    
+// }
+
+// multiple condition check
+const userLoggedIn = true
+const debitCard = true
+
+// if(userLoggedIn && debitCard & 2==2){
+//     console.log(`Allowed to buy course`);
+// }
+
+const loggedInGoogle = false
+const loggedInEmail = true
+
+// if (loggedInGoogle || loggedInEmail) {
+//     console.log("Allow to Login the website");
+// }
+
