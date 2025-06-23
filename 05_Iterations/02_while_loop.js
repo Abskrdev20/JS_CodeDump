9a94f09a2850a7081b7abef9a1364e5cb48115c1
Abhishek Kumar <absk.devopsmastero@gmail.com>
2025-06-23 14:25:11 +0000

    While loop in JS

---
+++ 05_Iterations/02_while_loop.js
@@ -0,0 +1,24 @@
+// While Loop in Js
+
+// let index = 0;
+// while (index<=10) {
+//     console.log(`Even no b/w 1-10 : ${index}`);
+//     index = index + 2
+// }
+
+// const mySubjects = ["pij","dbms","sql","pslp"]
+// let idx = 0
+// while(idx<mySubjects.length){
+//     console.log(idx)
+//     idx++
+// }
+
+// DO WHILE LOOP
+
+// let marks = 1 //generally
+// let marks = 15 // even now do stat work but while not
+// do {
+//     console.log(`Current score is ${marks}`);
+//     marks++
+// } while (marks<=5);
+
